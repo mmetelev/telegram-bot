961308910b3c6973cf76f719009bd9c5151b9b06
Maksim <93005551+mmetelev@users.noreply.github.com>
2022-10-20 22:35:22 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.11.0rc2-slim
+
+EXPOSE 8081/tcp
+
+WORKDIR /usr/src/app
+COPY . .
+
+RUN apt update && \
+    apt install --no-install-recommends -y build-essential curl && \
+    /usr/local/bin/python -m pip install --upgrade pip && \
+    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python - && \
+    export PATH="/root/.local/bin:$PATH" && \
+    poetry config virtualenvs.create false && \
+    poetry install --no-dev && \
+    poetry cache clear --no-interaction --all pypi && \
+    curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python - --uninstall && \
+    apt-get -y remove build-essential curl && apt-get -y autoremove --purge && apt-get -y clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /var/cache/apt
+
+ENTRYPOINT ["python", "-m", "bot"]
---
+++ bot/handlers.py
@@ -0,0 +1,78 @@
+from aiogram import types
+from bot.loader import bot
+from bot.loader import db
+from bot.loader import dp
+
+
+@dp.message_handler(commands="start")
+async def start_message(message: types.Message) -> None:
+    """welcome message."""
+    if await db.verification(message.from_user.id):
+        await bot.send_message(message.chat.id, "👋 Hello, I remember you.")
+    else:
+        if message.from_user.first_name != "None":
+            name = message.from_user.first_name
+        elif message.from_user.username != "None":
+            name = message.from_user.username
+        elif message.from_user.last_name != "None":
+            name = message.from_user.last_name
+        else:
+            name = ""
+        await db.add_user(message.from_user.id, name, message.from_user.locale.language_name)
+        await bot.send_message(message.chat.id, "ℹ️ <b>[About]\n</b> Bot is a template for future projects.")
+
+
+@dp.message_handler(commands=("help", "info", "about"))
+async def give_info(message: types.Message) -> None:
+    """the target of this bot."""
+    await bot.send_message(message.chat.id, "ℹ️ <b>[About]\n</b> Bot is a template for future projects.")
+
+
+@dp.message_handler(commands="contacts")
+async def give_contacts(message: types.Message) -> None:
+    """ссылка на код проекта."""
+    btn_link = types.InlineKeyboardButton(
+        text="Go to GitHub.", url="https://github.com/donBarbos/telegram-bot-template"
+    )
+    keyboard_link = types.InlineKeyboardMarkup().add(btn_link)
+    await bot.send_message(
+        message.chat.id,
+        "👨‍💻 the project code is available on Github.",
+        reply_markup=keyboard_link,
+    )
+
+
+@dp.message_handler(commands="settings")
+async def give_settings(message: types.Message) -> None:
+    """справка по настройкам."""
+    name = await db.get_name(message.from_user.id)
+    lang = await db.get_lang(message.from_user.id)
+    btn_name = types.InlineKeyboardButton(text=f"name: {name}", callback_data="name")
+    btn_lang = types.InlineKeyboardButton(text=f"language: {lang}", callback_data="lang")
+    keyboard_settings = types.InlineKeyboardMarkup().add(btn_name, btn_lang)
+    await bot.send_message(message.chat.id, "⚙️ eSettings", reply_markup=keyboard_settings)
+
+
+@dp.callback_query_handler(lambda c: c.data == "name")
+async def alter_name(callback_query: types.CallbackQuery) -> None:
+    await bot.send_message(callback_query.id, "How should I address you?")
+    await bot.answer_callback_query(callback_query.id)
+
+
+@dp.callback_query_handler(lambda c: c.data == "lang")
+async def alter_lang(callback_query: types.CallbackQuery) -> None:
+    await bot.send_message(callback_query.id, "Choose language:")
+    await bot.answer_callback_query(callback_query.id, "Choose language:")
+
+
+@dp.message_handler(content_types="text")
+async def text_handler(message: types.Message) -> None:
+    await bot.send_message(message.chat.id, "Text processing can take place here.")
+
+
+@dp.message_handler()
+async def unknown_message(message: types.Message) -> None:
+    if not message.is_command():
+        await bot.send_message(message.chat.id, "❌ I don't know how to work with this format.")
+    else:
+        await message.answer("❌ Invalid command.")
